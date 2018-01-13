e9c7eef1723bc07f198a8df2a0dfba3e34e3aa45
Kateryna Peikova <peikovakate@gmail.com>
2018-01-13 21:39:16 +0200

    added properties to product dtos

--- firstShop.API/Dtos/ProductDetailedDto.cs
+++ firstShop.API/Dtos/ProductDetailedDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using firstShop.API.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,11 @@ namespace firstShop.API.Dtos
 {
     public class ProductDetailedDto
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public Category ProductCategory { get; set; }
+        public bool IsAvailable { get; set; }
     }
 }
--- firstShop.API/Dtos/ProductForListDto.cs
+++ firstShop.API/Dtos/ProductForListDto.cs
@@ -7,5 +7,8 @@ namespace firstShop.API.Dtos
 {
     public class ProductForListDto
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
     }
 }
