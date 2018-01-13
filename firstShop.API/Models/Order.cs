69fe19ded7ec2d60e17c85b324924305ca29bf6d
Kateryna Peikova <peikovakate@gmail.com>
2018-01-13 21:07:15 +0200

    Added some Product, Order, Category models

---
+++ firstShop.API/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace firstShop.API.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+        public string Description { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
