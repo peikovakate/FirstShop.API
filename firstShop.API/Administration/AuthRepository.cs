610a0328b1c8c84f6936261c2fbd5a6478921da3
Kateryna Peikova <peikovakate@gmail.com>
2018-01-14 15:15:23 +0200

    added user to data context, added UserExists method logic

---
+++ firstShop.API/Administration/AuthRepository.cs
@@ -0,0 +1,38 @@
+﻿using firstShop.API.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using firstShop.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace firstShop.API.Administration
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _context;
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Task<User> Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> Register(User user, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> UserExists(string username)
+        {
+            if (await _context.Users.AnyAsync(u => u.Username == username))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- firstShop.API/Data/DataContext.cs
+++ firstShop.API/Data/DataContext.cs
@@ -17,5 +17,6 @@ namespace firstShop.API.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Order> Orders { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
