d909c0e21a7ce2943717395c398a14bade2e689c
Kateryna Peikova <peikovakate@gmail.com>
2018-01-14 15:09:14 +0200

    added IAuthRepository interface

---
+++ firstShop.API/Administration/IAuthRepository.cs
@@ -0,0 +1,15 @@
+﻿using firstShop.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace firstShop.API.Administration
+{
+    public interface IAuthRepository
+    {
+        Task<User> Register(User user, string password);
+        Task<User> Login(string username, string password);
+        Task<bool> UserExists(string username);
+    }
+}
